5b71d18eb4392c483281787e7ae99068b1f2596b
Ryo Shibayama <j02521@gmail.com>
2016-12-28 18:20:48 +0900

    add test case

--- cli_test.go
+++ cli_test.go
@@ -22,3 +22,19 @@ func TestRun_versionFlag(t *testing.T) {
 		t.Errorf("Output=%q, want %q", errStream.String(), expected)
 	}
 }
+
+func TestRun_withoutVersionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("awesome-cli", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus=%d, want %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("Do awesome working")
+	if !strings.Contains(outStream.String(), expected) {
+		t.Errorf("Output=%q, want %q", errStream.String(), expected)
+	}
+}
