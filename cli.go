708373046d5df5366af1ab85a347348a7d696d11
Ryo Shibayama <j02521@gmail.com>
2016-12-28 17:07:56 +0900

    Add sample codes

---
+++ cli.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+const (
+	ExitCodeOK             = iota
+	ExitCodeParseFlagError // = iota
+)
+
+type CLI struct {
+	// both are io.Writer
+	outStream, errStream io.Writer
+}
+
+// CLI structure as pointer receiver
+func (c *CLI) Run(args []string) int {
+	var version bool
+	flags := flag.NewFlagSet("awesome-cli", flag.ContinueOnError)
+	flags.SetOutput(c.errStream)
+	flags.BoolVar(&version, "version", false, "Print version information and quit")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeParseFlagError
+	}
+
+	if version {
+		fmt.Fprintf(c.errStream, "awesome-cli version %s\n", Version)
+		return ExitCodeOK
+	}
+
+	fmt.Fprint(c.outStream, "Do awesome workn")
+
+	return ExitCodeOK
+
+}
